0be31ecab30a12a406fc7c1f4c8bc50262eb0427
Jiaheng Ge <gejiaheng@gmail.com>
2020-11-24 19:35:52 -0800

    Add loading, error and retry to collection list

--- app/src/main/java/com/melodie/parotia/ui/collection/list/CollectionListViewModel.kt
+++ app/src/main/java/com/melodie/parotia/ui/collection/list/CollectionListViewModel.kt
@@ -2,7 +2,9 @@ package com.melodie.parotia.ui.collection.list
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.melodie.parotia.domain.collection.GetFeaturedCollectionUseCase
 import com.melodie.parotia.model.Collection
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +12,6 @@ import kotlinx.coroutines.flow.Flow
 class CollectionListViewModel @ViewModelInject constructor(
     getFeaturedCollectionUseCase: GetFeaturedCollectionUseCase
 ) : ViewModel() {
-    val collections: Flow<PagingData<Collection>> = getFeaturedCollectionUseCase(Unit)
+    val collections: Flow<PagingData<Collection>> =
+        getFeaturedCollectionUseCase(Unit).cachedIn(viewModelScope)
 }
