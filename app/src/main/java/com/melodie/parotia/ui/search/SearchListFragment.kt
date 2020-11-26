7c466ef0d2abd6548613a91681da5c7b21cf7cdc
Jiaheng Ge <gejiaheng@gmail.com>
2020-11-25 20:35:36 -0800

    Add loading, error and retry to search result list

--- app/src/main/java/com/melodie/parotia/ui/search/SearchListFragment.kt
+++ app/src/main/java/com/melodie/parotia/ui/search/SearchListFragment.kt
@@ -5,13 +5,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.paging.LoadState
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.melodie.parotia.databinding.FragmentSearchListBinding
+import com.melodie.parotia.ui.list.ParotiaLoadStateAdapter
 
 abstract class SearchListFragment<T : Any, VH : RecyclerView.ViewHolder> : Fragment() {
     protected lateinit var binding: FragmentSearchListBinding
@@ -25,9 +29,7 @@ abstract class SearchListFragment<T : Any, VH : RecyclerView.ViewHolder> : Fragm
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchListBinding.inflate(inflater, container, false)
-        setupRecyclerView(binding.recyclerView)
-        adapter = createAdapter()
-        binding.recyclerView.adapter = adapter
+
         return binding.root
     }
 
@@ -42,6 +44,29 @@ abstract class SearchListFragment<T : Any, VH : RecyclerView.ViewHolder> : Fragm
                 }
             }
         )
+
+        setupRecyclerView(binding.recyclerView)
+        adapter = createAdapter()
+        binding.recyclerView.adapter = adapter.withLoadStateFooter(
+            ParotiaLoadStateAdapter { adapter.retry() }
+        )
+        binding.setRetryClick { adapter.retry() }
+        adapter.addLoadStateListener { loadState ->
+            binding.recyclerView.isVisible =
+                loadState.source.refresh is LoadState.NotLoading || adapter.itemCount > 0
+
+            binding.progressBar.isVisible =
+                loadState.source.refresh is LoadState.Loading && adapter.itemCount == 0
+            binding.retryButton.isVisible =
+                loadState.source.refresh is LoadState.Error && adapter.itemCount == 0
+
+            // Toast on any error
+            val errorState = loadState.source.refresh as? LoadState.Error
+                ?: loadState.source.append as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(context, it.error.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     abstract fun setupRecyclerView(view: RecyclerView)
