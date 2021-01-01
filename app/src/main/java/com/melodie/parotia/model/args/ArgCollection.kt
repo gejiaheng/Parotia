32a8e4673a7ba34608650b4d75cecc5eb4d553ec
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-01 15:29:24 -0800

    Comment

--- app/src/main/java/com/melodie/parotia/model/args/ArgCollection.kt
+++ app/src/main/java/com/melodie/parotia/model/args/ArgCollection.kt
@@ -3,6 +3,9 @@ package com.melodie.parotia.model.args
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+/**
+ * Argument model object passed to [com.melodie.parotia.ui.collection.detail.CollectionFragment].
+ */
 @Parcelize
 data class ArgCollection(
     val id: Long,
